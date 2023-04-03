4ffac02c6dc3a00d726dad87ba7935e775348652
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-04-04 01:24:12 +0200

    console code added

--- management-app/src/Box/BoxStaffing.js
+++ management-app/src/Box/BoxStaffing.js
@@ -11,6 +11,7 @@ export default function BoxStaffing(props) {
         }}
     >
 
+
         <div className="box"
         style={{
             position: "relative",
--- management-app/src/Interface/Staffing.js
+++ management-app/src/Interface/Staffing.js
@@ -1,73 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "../StaffingStyle.css";
 import BoxStaffing from '../Box/BoxStaffing';
 import OptionBtn from '../Button/OptionBtn';
 import AlignMenuShorter from '../Menu/AlignMenuShorter';
 import RandomBox from '../RandomBox';
 
-export default function Staffing(props) {
-  return (
+class Staffing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      project: "switch",
+    };
+  }
+
+  switchProject = (e) => {
     
-    <div className="staffing">
-      
+    console.log(this.state.project);
+
+  };
+
+  render() {
+    return (
+      <div className="staffing">
         <div className='menu-right'>
-
-        <AlignMenuShorter
-        bottom= "40px"
-      
-        buttonText = "Project"
-        links = "/"
-
-        buttonText1 = "Staffing"
-        links1 = "/"
-
-        buttonText2 = "Calendar"
-        links2 = "/"
-
-        buttonText3 = "Finance"
-        links3 = "/"
-
-        buttonText4 = "Vacancy"
-        links4 = "/"
-
-        buttonText5 = "Mail"
-        links5 = "/"
-        />
-
-        </div>
-        
-        <BoxStaffing/>
-
-        <div style={{display: 'flex', alignItems: 'center', marginLeft: '30%'}}>
-        <div style={{marginRight:'10%'}}>
-        <OptionBtn/>
-        </div>
-        <div style={{marginRight:'10%'}}>
-        <OptionBtn/>
-        </div>
-        <div style={{marginRight:'5'}}>
-        <OptionBtn/>
-        </div>
+          <AlignMenuShorter
+            bottom="40px"
+            buttonText="Project"
+            links="/"
+            buttonText1="Staffing"
+            links1="/"
+            buttonText2="Calendar"
+            links2="/"
+            buttonText3="Finance"
+            links3="/"
+            buttonText4="Vacancy"
+            links4="/"
+            buttonText5="Mail"
+            links5="/"
+          />
         </div>
 
-        <div style={{marginTop:'2%', marginLeft:"30%"}}>
-        <RandomBox size="150px" width={'73%'} radius={'22px'} />
-        </div>
-        <div style={{marginTop:'10px', marginLeft:"30%"}}>
-        <RandomBox size="150px" width={'73%'} radius={'22px'} />
-        </div>
-        <div style={{marginTop:'10px', marginLeft:"30%"}}>
-        <RandomBox size="150px" width={'73%'} radius={'22px'} />
-        </div>
-        <div style={{marginTop:'10px', marginLeft:"30%"}}>
-        <RandomBox size="150px" width={'73%'} radius={'22px'} />
+        <div className='containerStaff' style={{ marginLeft: "30%" }}>
+          <div className="circle-arrow" onClick={this.switchProject}>
+            <div className="arrow"></div>
+          </div>
         </div>
 
+        <BoxStaffing />
 
-
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: '30%' }}>
+          <div style={{ marginRight: '10%' }}>
+            <OptionBtn />
+          </div>
+          <div style={{ marginRight: '10%' }}>
+            <OptionBtn />
+          </div>
+          <div style={{ marginRight: '5%' }}>
+            <OptionBtn />
+          </div>
         </div>
-          
-        
 
-)
+        <div style={{ marginTop: '2%', marginLeft: "30%" }}>
+          <RandomBox size="150px" width={'73%'} radius={'22px'} />
+        </div>
+
+        <div style={{ marginTop: '10px', marginLeft: "30%" }}>
+          <RandomBox size="150px" width={'73%'} radius={'22px'} />
+        </div>
+
+        <div style={{ marginTop: '10px', marginLeft: "30%" }}>
+          <RandomBox size="150px" width={'73%'} radius={'22px'} />
+        </div>
+
+        <div style={{ marginTop: '10px', marginLeft: "30%" }}>
+          <RandomBox size="150px" width={'73%'} radius={'22px'} />
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Staffing;
+
