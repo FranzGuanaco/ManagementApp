71eab7ad6c58a65603f1a40689a8be18b9e2ed90
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-04-05 00:05:07 +0200

    alert boxes added

--- management-app/src/Interface/Parameters.js
+++ management-app/src/Interface/Parameters.js
@@ -3,11 +3,57 @@ import '../Style/ParametersStyle.css'
 import AlignMenuShorter from '../Menu/AlignMenuShorter';
 
 class Parameters extends Component {
+
     constructor(props) {
         super(props);
 
+        this.state = {
+          isModalAdminOpen: false,
+            username: '',
+            password: '',
+          isModalPasswordOpen: false,
+            username2: '',
+            password2: ''
+        };
+
+      }
+    
+      openModalAdmin = () => {
+        this.setState({ isModalAdminOpen: true });
+      }
+    
+      closeModalAdmin = () => {
+        this.setState({ isModalAdminOpen: false });
+      }
+
+      openModalPassword = () => {
+        this.setState({ isModalPasswordOpen: true });
+      }
+    
+      closeModalPassword = () => {
+        this.setState({ isModalPasswordOpen: false });
+      }
+    
+      handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+      }
+    
+      handleSubmit = () => {
+        const { username, password } = this.state;
+        // Faites quelque chose avec les informations de l'utilisateur
+        console.log(`Nom d'utilisateur : ${username}, Mot de passe : ${password}`);
+        this.closeModalAdmin();
+      }
+
+        LogOutRequest = (e) => {
+            alert('êtes vous sûr de vouloir vous déconnecter ?')
+
       }
   render() {
+    const { isModalAdminOpen, username, password } = this.state;
+    const { isModalPasswordOpen, username2, password2 } = this.state;
+
     return (
       <div className="staffing">
         <div className='menu-right'>
@@ -27,21 +73,54 @@ class Parameters extends Component {
             links5="/"
           />
         </div>
-        <div class="Paramcontainer">
+        <div class="Paramcontainer" onClick={this.openModalPassword}>
       <div class="OptionTitle">Change Password</div>
       <div class="OptionText">{this.props.text || Parameters.defaultProps.text}</div>
     </div>
    
-    <div class="Paramcontainer">
+    <div class="Paramcontainer" onClick={this.openModalAdmin}>
       <div class="OptionTitle">Add Admin</div>
       <div class="OptionText">{this.props.text || Parameters.defaultProps.text}</div>
     </div>
    
-    <div class="Paramcontainer">
+    <div class="Paramcontainer" onClick={this.LogOutRequest}>
       <div class="OptionTitle">Log Out</div>
       <div class="OptionText">{this.props.text || Parameters.defaultProps.text}</div>
     </div>
-      </div>
+
+
+
+    {isModalAdminOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={this.closeModalAdmin}>&times;</span>
+              <h2>Ajouter un compte</h2>
+              <label htmlFor="username">Nom d'utilisateur :</label>
+              <input type="text" id="username" className='inputAddAdmin' name="username" value={username} onChange={this.handleInputChange} />
+              <label htmlFor="password">Mot de passe :</label>
+              <input type="password" id="password" className='inputAddAdmin' name="password" value={password} onChange={this.handleInputChange} />
+              <button onClick={this.handleSubmit}>Valider</button>
+            </div>
+          </div>
+        )}
+
+{isModalPasswordOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={this.closeModalPassword}>&times;</span>
+              <h2>Mot de passe:</h2>
+              <label htmlFor="username">Ancien mot de passe :</label>
+              <input type="text" id="username2" className='inputAddAdmin' name="username2" value={username2} onChange={this.handleInputChange} />
+              <label htmlFor="password">Nouveau mot de passe :</label>
+              <input type="password" id="password2" className='inputAddAdmin' name="password2" value={password2} onChange={this.handleInputChange} />
+              <button onClick={this.handleSubmit}>Valider</button>
+            </div>
+          </div>
+        )}
+           
+   </div>
+
+   
     );
   }
 }
