733a39612d8d8e31bbdfe5666c5a5b3b777a64eb
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-04-17 23:31:25 +0200

    user connection done with the API and MySQL

--- management-app/database.js
+++ management-app/database.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+const express = require('express');
+const cors = require('cors');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -7,11 +9,54 @@ const connection = mysql.createConnection({
   database: 'appreact'
 });
 
-connection.query('SELECT * FROM admin', (error, results, fields) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('bien joué');
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database with id ' + connection.threadId);
+});
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+connection.query('SELECT * FROM Admin', function(err, results, fields) {
+  console.log(results);
+});
+
+app.post('/auth/login', function(req, res) {
+  const { username, password } = req.body;
+
+  connection.query(
+    "SELECT * FROM Admin WHERE username = ? AND password = ?",
+    [username, password],
+    function(err, results, fields) {
+      if (err) {
+        res.status(500).send({ error: 'Internal Server Error' });
+        return;
+      }
+
+      if (results.length > 0) {
+        res.status(200).send({ success: 'User authenticated' });
+      } else {
+        res.status(401).send({ error: 'Invalid credentials' });
+      }
     }
-  });
-  
+  );
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
--- management-app/src/Interface/BoxId.js
+++ management-app/src/Interface/BoxId.js
@@ -21,14 +21,40 @@ class BoxId extends React.Component {
     });
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state.userName);
-  }
-
   handleLogin = () => {
     window.location.href = "/Homepage";
   }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    const { userName } = this.state;
+  
+    // Récupérer le mot de passe du formulaire
+    const password = event.target.password.value;
+  
+    // Envoyer les informations de connexion au serveur
+    try {
+      const response = await fetch('http://localhost:3001/auth/login', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+        },
+      body: JSON.stringify({ username: userName, password }),
+      });
+
+  
+      if (response.ok) {
+        console.log('User authenticated');
+        this.handleLogin();
+      } else {
+        console.log('Invalid credentials');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+  
 
   render() {
    
@@ -41,7 +67,7 @@ class BoxId extends React.Component {
           <input type="text" id="username" name="userName" value={userName} placeholder="" onChange={this.handleInputChange} />
           <label htmlFor="password">{this.props.title2}</label>
           <input type="password" id="password" name="password" />
-          <input type="submit" value="Se connecter" onClick={this.handleLogin} />
+          <input type="submit" value="Se connecter"/>
         </form>
 
       </div>
@@ -55,4 +81,9 @@ BoxId.defaultProps = {
 }
 
 export default BoxId;
+
+
+
+
+
 
