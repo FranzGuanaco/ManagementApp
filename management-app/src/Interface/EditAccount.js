9ea0496d19519925a53c29b7626c99812d40727b
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-05-02 23:19:51 +0200

    back Editbuton

--- management-app/src/Interface/EditAccount.js
+++ management-app/src/Interface/EditAccount.js
@@ -1,104 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import AlignMenu from '../Menu/AlignMenu';
-import BackButton from '../Button/BackButton'
-import Form from '../Form/Form';
-import Pic from '../Picture/Pic';
 import ParamPic from './ParamPic';
-import { Link } from 'react-router-dom'
-import "../App.css";
+import Form from '../Form/Form';
+import BackButton from '../Button/BackButton';
+import Pic from '../Picture/Pic';
 
-function EditAccount () {
+function EditAccount() {
+  const [employees, setEmployees] = useState([]);
 
-    const Backfunc = () => { // Ajout de const devant handleLogin pour définir une fonction fléchée
-        window.location.href = "/Employee details";
-    }
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/EmployeeDetails')
+      .then((response) => {
+        console.log('reussi');
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.log('raté');
+      });
+  }, []);
 
-    return (
+  const Backfunc = () => {
+    window.location.href = '/Employee details';
+  };
 
-        <div className="home">
+  return (
+    <div className="home">
+      <AlignMenu
+        bottom="44px"
+        buttonText="Home"
+        links="/Homepage"
+        buttonText1="Edit"
+        links1="/EditAccount"
+        buttonText2="Staffing"
+        links2="/"
+        buttonText3="Calendar"
+        links3="/"
+        buttonText4="Employee"
+        links4="/"
+        buttonText5="Vacancy"
+        links5="/"
+        buttonText6="Mail"
+        links6="/"
+      />
 
-    <AlignMenu
-       bottom= "44px"
-
-       buttonText = "Home"
-       links ="/Homepage"
-
-       buttonText1 = "Edit"
-       links1 ="/EditAccount"
-
-       buttonText2 = "Staffing"
-       links2 ="/"
-
-       buttonText3 = "Calendar"
-       links3 ="/"
-
-       buttonText4 = "Employee"
-       links4 ="/"
-
-       buttonText5 = "Vacancy"
-       links5 ="/"
-
-       buttonText6 = "Mail"
-       links6 ="/"
-    />
-
-    <div className='menu-left'>
-            <Link to='/Parameters'>
-            <ParamPic/>
-            </Link>
-        </div>
-    
-    <div className="container">
-    <div className="randombox-container">
-    <Form size="75px" width={'230px'} radius={'18px'} />
-    <div style={{marginLeft: '40px'}}>
-    <Form size="75px" width={'230px'} radius={'18px'} />
-    </div>
-    </div>
-
-    <div style={{marginTop:'10px', marginRight:'1000px'}}>
-    <Form size="75px" width={'500px'} radius={'22px'} text='ooo'/>
-    </div>
-    <div style={{marginTop:'10px', marginRight:'1000px'}}>
-    <Form size="75px" width={'500px'} radius={'22px'} text='ooo'/>
-    </div>
-
-    <div className="randombox-container">
-    <div style={{marginTop:'10px'}}>
-    <Form size="75px" width={'500px'} radius={'22px'} />
-    </div>
-    <div style={{marginLeft: '50px', marginTop:'10px'}}>
-    <Form size="75px" width={'500px'} radius={'22px'} />
-    </div>
-    </div>
-
-    <div style={{marginTop:'10px', marginRight:'1000px'}}>
-    <Form size="75px" width={'1070px'} radius={'22px'} text='ooo'/>
-    </div>
-    <div style={{marginTop:'10px', marginRight:'1000px'}}>
-    <Form size="75px" width={'1070px'} radius={'22px'} text='ooo'/>
-    </div>
-    <div style={{marginTop:'10px', marginRight:'1000px'}}>
-    <Form size="75px" width={'1070px'} radius={'22px'} text='ooo'/>
-    </div>
-
-    <div style={{marginTop:'20px'}}>
-    <BackButton onClickBack={Backfunc} style={{margin: 'auto'}} />
-    </div>
-
-    <div className="pic">
-    <div style={{bottom: '20px', marginLeft: '50%' }}>
-    <Pic/>
-    <Form size="30px" width={'100px'} radius={'22px'} />
-    </div>
+      <div className="menu-left">
+        <Link to="/Parameters">
+          <ParamPic />
+        </Link>
       </div>
-        </div>
-        </div>
 
+      <div className="container">
+        {employees.map((employee) => (
+          <>
+            <div className="randombox-container">
+              <Form size="75px" width="230px" radius="18px" text={employee.Firstname} />
+              <div style={{ marginLeft: '40px' }}>
+                <Form size="75px" width="230px" radius="18px" text={employee.Firstname} />
+              </div>
+            </div>
 
+            <div style={{ marginTop: '10px', marginRight: '1000px' }}>
+              <Form size="75px" width="500px" radius="22px" text={employee.Firstname} />
+            </div>
+            <div style={{ marginTop: '10px', marginRight: '1000px' }}>
+              <Form size="75px" width="500px" radius="22px" text={employee.Firstname} />
+            </div>
 
-    );
+            <div className="randombox-container">
+              <div style={{ marginTop: '10px' }}>
+                <Form size="75px" width="500px" radius="22px" text={employee.Firstname} />
+              </div>
+              <div style={{ marginLeft: '50px', marginTop: '10px' }}>
+                <Form size="75px" width="500px" radius="22px" text={employee.Firstname} />
+              </div>
+            </div>
+
+            <div style={{ marginTop: '10px', marginRight: '1000px' }}>
+              <Form size="75px" width="1070px" radius="22px" text={employee.Firstname} />
+            </div>
+            <div style={{ marginTop: '10px', marginRight: '1000px' }}>
+              <Form size="75px" width="1070px" radius="22px" text={employee.Firstname} />
+            </div>
+            <div style={{ marginTop: '10px', marginRight: '1000px' }}>
+              <Form size="75px" width="1070px" radius="22px" text={employee.Firstname} />
+            </div>
+
+            <div style={{ marginTop: '20px' }}>
+              <BackButton onClickBack={Backfunc} style={{ margin: 'auto' }} />
+            </div>
+
+            <div className="pic">
+              <div style={{ bottom: '20px', marginLeft: '50%' }}>
+                <Pic />
+                <Form size="30px" width="100px" radius="22px" text={employee.Firstname} />
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
-
 
 export default EditAccount;
