34275b32bbb03ffe5213897402fc5c6aef4d7d28
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-08-11 00:33:37 +0200

    homepage done

--- management-app/src/Box/BoxEmployee.js
+++ management-app/src/Box/BoxEmployee.js
@@ -9,6 +9,7 @@ import Homepage from "../Interface/Homepage";
 
 function BoxEmployee({ onEmployeeClick }) {
   const [employees, setEmployees] = useState([]);
+  const [boxWidth, setBoxWidth] = useState(300); // Largeur de la boîte par défaut
 
   useEffect(() => {
     axios
@@ -19,22 +20,74 @@ function BoxEmployee({ onEmployeeClick }) {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
 
-  const data = "Hello Everyone"; // Définissez la valeur ici
+   // Mettre à jour la largeur de la boîte en fonction de la largeur de la fenêtre lors du chargement initial et lorsqu'elle est redimensionnée
+   const updateBoxWidth = () => {
+    const windowWidth = window.innerWidth;
+    const maxWidth = 1000; // Largeur maximale souhaitée
+    const minWidth = 300; // Largeur minimale souhaitée
+    const width = Math.max(minWidth, Math.min(maxWidth, windowWidth - 100)); // Calculer la largeur en fonction de la taille de la fenêtre
+    setBoxWidth(width);
+  };
+
+  // Écouter l'événement de redimensionnement de la fenêtre
+  window.addEventListener("resize", updateBoxWidth);
+
+  // Appeler la fonction de mise à jour de la largeur de la boîte lors du chargement initial
+  updateBoxWidth();
+
+  // Nettoyer l'écouteur d'événement lorsque le composant est démonté
+  return () => {
+    window.removeEventListener("resize", updateBoxWidth);
+  };
+}, []);
 
   return (
     <div>
       {employees.map((employee) => (
+        <div 
+        onClick={() => onEmployeeClick(employee.id)}
+          key={employee.id}
+          className="box"
+          style={{
+            position: "relative",
+            marginBottom: "7%",
+            justifyContent: "center",
+            width: `${boxWidth}px`, // Utiliser la valeur de la largeur de la boîte mise à jour
+          }}
+        >
 
         <div style={{ top: "20%"}}>
           <div key={employee.id}>
 
-          <p onClick={() => onEmployeeClick(employee.id)}>Le test : {employee.id}</p>
-            
-           
+         
         </div>
         </div>
+        <div style={{ position: "absolute", top: "15%", left: "4%" }}>
+            <Pic />
+          </div>
+          <div
+            style={{ position: "relative", marginLeft: "70%", top: "10%" }}
+          >
+            <EditButton
+              width="36%"
+              height="100%"
+              minWidth="100px"
+              minHeight="90px"
+            />
+          </div>
+          <div style={{ top: "20%"}}>
+            <TextDataEmployee
+              left="25%"
+              width="45%"
+              Nom={`${employee.Firstname} ${employee.Surname}`}
+              poste={employee.position}
+              contact={employee.Mail}
+              disponibilité={employee.Availability}
+              cost={employee.Income}
+            />
+          </div>
+          </div>
       ))}
     </div>
   );
--- management-app/src/Interface/Homepage.js
+++ management-app/src/Interface/Homepage.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import BoxEmployee from '../Box/BoxEmployee';
@@ -5,18 +6,26 @@ import AlignMenuShorter from '../Menu/AlignMenuShorter';
 import BoxNews from '../Box/BoxNews'
 import { Link } from "react-router-dom";
 import ParamPic from './ParamPic';
-import ViewAccount from './ViewAccount';
 
 export default function Homepage() {
   const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
   const redirectUrl = `/Employee details?=${selectedEmployeeId}`;
 
+  const [username, setUsername] = useState('');
+  const location = useLocation();
+
   useEffect(() => {
-    console.log("result:", selectedEmployeeId);
+     console.log("result:", selectedEmployeeId);
     if(selectedEmployeeId !== null){
     window.location.href = redirectUrl;
     }
   }, [selectedEmployeeId]);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const usernameParam = queryParams.get('username');
+    setUsername(usernameParam);
+  }, [location]);
 
   const handleEmployeeClick = (employeeId) => {
     setSelectedEmployeeId(employeeId);
@@ -25,13 +34,47 @@ export default function Homepage() {
   return (
     <div className="home">
       <div className='menu-left'>
+      <Link to='/Parameters'>
+        <ParamPic/>
+        </Link>
+        </div>
+
+        <div className='menu-right'>
+        <AlignMenuShorter
+        bottom= "40px"
+      
+        buttonText = "Project"
+        links = "/"
+
+        buttonText1 = "Staffing"
+        links1 = "/"
+
+        buttonText2 = "Calendar"
+        links2 = "/"
+
+        buttonText3 = "Finance"
+        links3 = "/"
+
+        buttonText4 = "Vacancy"
+        links4 = "/"
+
+        buttonText5 = "Mail"
+        links5 = "/"
+        />
+        <div style={{ left: "500px" }}>
+        <div style={{ padding: "10px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          
+          
         
           <BoxEmployee onEmployeeClick={handleEmployeeClick} />
-                   
+         
+        </div>
       </div>
-     
+    </div>
     </div>
   );
 }
+
+
 
 
